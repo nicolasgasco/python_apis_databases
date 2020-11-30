dd81668904446a850eef0326780c409db2e41561
nicolasgasco <nicolas.gasco92@gmail.com>
2020-11-30 14:30:40 +0100

    Completed exercise 2

--- apis/Exercise_02.py
+++ apis/Exercise_02.py
@@ -3,3 +3,13 @@ Building on the previous example, create a list of all of the emails of the user
 the list to the console.
 
 '''
+
+import requests
+from pprint import pprint
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+response = requests.get(base_url)
+
+data = response.json()
+emails = [element["email"] for element in data["data"]]
+print(emails)
