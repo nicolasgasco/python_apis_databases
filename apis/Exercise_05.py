c49034ca1ffabd40cb1fd02a6d839a068733eda9
nicolasgasco <nicolas.gasco92@gmail.com>
2020-11-30 14:31:21 +0100

    Completed exercise 5

--- apis/Exercise_05.py
+++ apis/Exercise_05.py
@@ -4,3 +4,17 @@ Write a program that makes a DELETE request to remove the user your create in a 
 Again, make a GET request to confirm that information has been deleted.
 
 '''
+
+
+import requests
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+response = requests.delete(base_url + "/205")
+
+print(f"Status code of POST: {response.status_code}")
+
+response = requests.get(base_url)
+print(f"Content: {response.content}")
+print(f"Status code of GET: {response.status_code}")
+
