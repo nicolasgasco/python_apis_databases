a51cc5a01e441b26ae749102a458587264df2fa0
nicolasgasco <nicolas.gasco92@gmail.com>
2020-11-30 14:30:55 +0100

    Completed exercise 3

--- apis/Exercise_03.py
+++ apis/Exercise_03.py
@@ -8,3 +8,21 @@ and create a user with your information.
 Make a GET request to confirm that your user information has been saved.
 
 '''
+
+import requests
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+body = {
+    "first_name": "James",
+    "last_name": "Bond",
+    "email": "james.bond@007.uk"
+}
+
+response = requests.post(base_url, json=body)
+
+print(f"Status code of POST: {response.status_code}")
+
+response = requests.get(base_url)
+print(f"Status code of GET: {response.status_code}")
+
